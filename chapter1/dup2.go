50656706b729da33cfc0b5998d93929f35728fa2
Altiera <altieraqq@outlook.com>
2025-10-02 16:59:50 +0500

    dup2

---
+++ chapter1/dup2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		CountLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprint(os.Stderr, "dup2: %v\n", err)
+				continue
+			} else {
+				CountLines(f, counts)
+				f.Close()
+			}
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func CountLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
